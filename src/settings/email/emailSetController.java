98bfd486f97d675674921e7a78f59a4825e7da73
unknown <tahir60652@gmail.com>
2018-01-19 19:35:41 +0500

    ARWIN

--- src/settings/email/emailSetController.java
+++ src/settings/email/emailSetController.java
@@ -3,21 +3,28 @@ package settings.email;
 import JCode.Toast;
 import JCode.fileHelper;
 import JCode.mySqlConn;
+import JCode.trayHelper;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
+import javafx.stage.*;
 import objects.ESetting;
 import objects.Users;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -65,13 +72,14 @@ public class emailSetController implements Initializable {
 
     @FXML
     void setAuto(ActionEvent event) {
-
+        inflateTextArea(1);
     }
 
     @FXML
     void setDisclaimer(ActionEvent event) {
-
+        inflateTextArea(2);
     }
+
 
     @FXML
     void onChoose(ActionEvent event) {
@@ -98,5 +106,30 @@ public class emailSetController implements Initializable {
         ESetting es = new ESetting(host, email, pass, fspath);
         sql.saveEmailSettings(es);
 
+    }
+
+    private void inflateTextArea(int c) {
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initStyle(StageStyle.UTILITY);
+        stage.setTitle("Auto Reply");
+        AnchorPane pane = new AnchorPane();
+        TextArea area = new TextArea();
+        area.setMinSize(500, 500);
+        pane.getChildren().add(area);
+        stage.setScene(new Scene(pane, 500, 500));
+        trayHelper tray = new trayHelper();
+        tray.createIcon(stage);
+        Platform.setImplicitExit(true);
+
+        stage.setOnHiding(event -> {
+            if (c == 1) {        //Auto Reply
+                System.out.println("Auto Reply");
+            } else if (c == 2) {    //Disclaimer
+                System.out.println("Disclaimer");
+            }
+        });
+
+        stage.show();
     }
 }
