c1360c73dbe3964896643b799023625b4620db62
Tahir Shakir <tahir60652@gmail.com>
2018-05-17 10:35:57 +0500

    lol

--- src/product/newProduct/NewProductController.java
+++ src/product/newProduct/NewProductController.java
@@ -13,22 +13,26 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import objects.ProductModule;
 import objects.ProductProperty;
 import product.ProductDashController;
 import product.view.ProductViewController;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class NewProductController implements Initializable {
-    
+
     @FXML
     private JFXTextField txt_name;
     @FXML
@@ -51,18 +55,18 @@ public class NewProductController implements Initializable {
     private JFXButton btn_add_module;
     @FXML
     private VBox vbox_modules;
-    
+
     public static char stInstance;
-    
+
     private ProductProperty product;
     private mySqlConn sql;
-    
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         sql = new mySqlConn();
         product = new ProductProperty();
         vbox_modules.setSpacing(10);
-        
+
         Image image = new Image(this.getClass().getResourceAsStream("/res/img/left-arrow.png"));
         btn_back.setGraphic(new ImageView(image));
         btn_back.setAlignment(Pos.CENTER_LEFT);
@@ -72,12 +76,12 @@ public class NewProductController implements Initializable {
                 ProductDashController.main_paneF.setCenter(
                         FXMLLoader.load(
                                 getClass().getClassLoader().getResource("product/view/plus-green.fxml")));
-                
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
-        
+
         image = new Image(this.getClass().getResourceAsStream("/res/img/plus-green.png"));
         btn_add_module.setGraphic(new ImageView(image));
         btn_add_module.setAlignment(Pos.CENTER_LEFT);
@@ -85,14 +89,14 @@ public class NewProductController implements Initializable {
         btn_add_module.setOnAction(event -> {
             HBox hBox = new HBox();
             hBox.setSpacing(5);
-            
+
             JFXTextField txt_name = new JFXTextField();
             txt_name.setAccessibleText("name");
             txt_name.setPromptText("Name");
             txt_name.setMaxWidth(100);
             txt_name.setMaxHeight(20);
             txt_name.getStyleClass().add("blackText");
-            
+
             TextArea txt_desc = new TextArea();
             txt_desc.setAccessibleText("desc");
             txt_desc.setPromptText("Description");
@@ -100,19 +104,20 @@ public class NewProductController implements Initializable {
             txt_desc.setMaxHeight(20);
             txt_desc.setWrapText(true);
             txt_desc.getStyleClass().add("blackText");
-            
+
             JFXButton btn_delete = new JFXButton("X");
             btn_delete.setOnAction(event1 -> vbox_modules.getChildren().remove(hBox));
-            
+
             hBox.getChildren().addAll(txt_name, txt_desc, btn_delete);
-            
+
             vbox_modules.getChildren().add(hBox);
         });
-        
+
         if (stInstance == 'N') {
             product = new ProductProperty();
             txt_heading.setText("New Product");
             btn_save.setText("Add");
+            btn_add_module.fire();
         } else if (stInstance == 'U') {
             btn_save.setText("Update");
             product = ProductViewController.staticProduct;
@@ -120,7 +125,22 @@ public class NewProductController implements Initializable {
             txt_heading.setText("Update Product");
         }
     }
-    
+
+    private void insertModules() {
+        List<ProductModule> modules = new ArrayList<>();
+        for (Node node : vbox_modules.getChildren()) {
+            ProductModule module = new ProductModule();
+            HBox box = (HBox) node;
+            for (Node n : box.getChildren()) {
+                if (n.getAccessibleText().equals("name")) {
+                    module.setName(((JFXTextField) n).getText().toString());
+                } else if (n.getAccessibleText().equals("desc")) {
+                    module.setDesc(((TextArea) n).getText().toString());
+                }
+            }
+        }
+    }
+
     private void populateDetails(ProductProperty newValue) {
         if (newValue == null)
             return;
@@ -128,7 +148,7 @@ public class NewProductController implements Initializable {
             txt_name.setText("");
         else
             txt_name.setText(newValue.getName());
-        
+
         txt_name.setText(product.getName());
         txt_price.setText(String.valueOf(product.getPrice()));
         txt_desc.setText(product.getDesc());
@@ -136,16 +156,16 @@ public class NewProductController implements Initializable {
             started_date.setValue(CommonTasks.createLocalDate(newValue.getStartedtimeStmp()));
         else
             started_date.setValue(null);
-        
+
     }
-    
-    
+
+
     public void saveChanges(ActionEvent actionEvent) {
         String name = txt_name.getText(),
                 price = txt_price.getText(),
                 desc = txt_desc.getText(),
                 started = String.valueOf(started_date.getValue());
-        
+
         if (name.equals("") || desc.equals("")) {
             Toast.makeText((Stage) btn_save.getScene().getWindow(), "Required Fields Are Empty");
             return;
@@ -167,9 +187,9 @@ public class NewProductController implements Initializable {
             Alert alert2 = new Alert(Alert.AlertType.CONFIRMATION, msg,
                     ButtonType.YES, ButtonType.NO);
             alert2.showAndWait();
-            
+
             if (alert2.getResult() == ButtonType.YES) {
-                
+
                 product.setName(name);
                 product.setPrice(Integer.parseInt(price));
                 product.setDesc(desc);
@@ -178,7 +198,7 @@ public class NewProductController implements Initializable {
                 else
                     product.setStartedtimeStmp(started);
                 product.setFreeze(false);
-                
+
                 switch (stInstance) {
                     case 'N': {
                         sql.insertProduct(product);
@@ -192,10 +212,12 @@ public class NewProductController implements Initializable {
                         break;
                     }
                 }
-                
+
             } else {
                 return;
             }
         }
     }
+
+
 }
