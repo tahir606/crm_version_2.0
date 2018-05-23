f89488874270fad19dc52a06f85aea683fdbe141
tahir606 <tahir60652@gmail.com>
2018-05-23 11:08:14 +0500

    lol

---
+++ src/objects/Lead.java
@@ -0,0 +1,13 @@
+package objects;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Lead {
+    
+    private SimpleIntegerProperty code;
+    private SimpleStringProperty firstName;
+    
+    public Lead() {
+    }
+}
